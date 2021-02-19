395f7aed12f7e862b34e374195ef402978b42e99
Groeboe <gerritdegraaff@gmail.com>
2021-02-19 12:53:57 +0100

    Revert "Initial commit"
    This reverts commit d47ff0fd

--- src/main/java/com/gerritdegraaff/lil/learningspring/data/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gerritdegraaff.lil.learningspring.data.repository;
-
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends CrudRepository<Room, Long> {
-}
